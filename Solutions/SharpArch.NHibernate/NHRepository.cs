72b8084990ee836b803af42e9e757b0b8a42bf4a
Antonio Milesi Bastos <antonio@milesibastos.com>
2013-11-04 18:31:31 -0200

    NHRepository<T> : IRepository<T>

---
+++ Solutions/SharpArch.NHibernate/NHRepository.cs
@@ -0,0 +1,165 @@
+namespace SharpArch.NHibernate
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
+
+    using global::NHibernate;
+    using global::NHibernate.Linq;
+    using global::NHibernate.Criterion;
+
+    using SharpArch.Domain;
+    using SharpArch.Domain.DomainModel;
+    using SharpArch.Domain.PersistenceSupport;
+    using SharpArch.NHibernate.Contracts.Repositories;
+    using System.Linq;
+
+    /// <summary>
+    ///     Provides a fully loaded DAO which may be created in a few ways including:
+    ///     * Direct instantiation; e.g., new GenericDao<Customer>
+    ///     * Spring configuration; e.g., <object id = "CustomerDao" type = "SharpArch.Data.NHibernateSupport.GenericDao&lt;CustomerAlias>, SharpArch.Data" autowire = "byName" />
+    /// </summary>
+    public class NHRepository<T> : IRepository<T>
+    {
+
+        #region Properties
+
+        protected virtual ISession Session
+        {
+            get
+            {
+                string factoryKey = SessionFactoryKeyHelper.GetKeyFrom(this);
+                return NHibernateSession.CurrentFor(factoryKey);
+            }
+        }
+
+        #endregion
+
+        #region IRepository<T> Members
+
+        public T this[object id]
+        {
+            get
+            {
+                var entity = Session.Get<T>(id);
+                return entity;
+            }
+        }
+
+        #endregion
+
+        #region ICollection<T> Members
+
+        public void Add(T entity)
+        {
+            try
+            {
+                this.Session.Save(entity);
+            }
+            catch
+            {
+                if (this.Session.IsOpen)
+                {
+                    this.Session.Close();
+                }
+
+                throw;
+            }
+
+            this.Session.Flush();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T entity)
+        {
+            var query = Session.Query<T>();
+
+            return query.Contains(entity);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            var query = Session.Query<T>();
+            var recordset = query.ToArray();
+
+            Array.Copy(recordset, 0, array, arrayIndex, Count);
+        }
+
+        public int Count
+        {
+            get
+            {
+                var query = Session.Query<T>();
+                return query.Count();
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+        public bool Remove(T entity)
+        {
+            Session.Delete(entity);
+            return true;
+        }
+
+        #endregion
+
+        #region IEnumerable<T> Members
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var query = Session.Query<T>();
+            return query.GetEnumerator();
+        }
+
+        #endregion
+
+        #region IEnumerable Members
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        #endregion
+
+        #region IQueryable Members
+
+        public Type ElementType
+        {
+            get
+            {
+                var query = Session.Query<T>();
+                return query.ElementType;
+            }
+        }
+
+        public System.Linq.Expressions.Expression Expression
+        {
+            get
+            {
+                var query = Session.Query<T>();
+                return query.Expression;
+            }
+        }
+
+        public IQueryProvider Provider
+        {
+            get
+            {
+                var query = Session.Query<T>();
+                return query.Provider;
+            }
+        }
+
+        #endregion
+
+    }
+}
