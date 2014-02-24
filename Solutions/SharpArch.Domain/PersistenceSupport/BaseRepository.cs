4fc3b5964454767ea952ae1bb70f0fc2582490f0
Antonio Milesi Bastos <antonio@milesibastos.com>
2014-02-24 20:30:51 -0300

    fix NHRepository

--- Solutions/SharpArch.Domain/PersistenceSupport/BaseRepository.cs
+++ Solutions/SharpArch.Domain/PersistenceSupport/BaseRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using SharpArch.Domain.DomainModel;
@@ -7,8 +8,6 @@ namespace SharpArch.Domain.PersistenceSupport
 {
     public abstract class BaseRepository<T> : EnumerableQuery<T>, IRepository<T>
     {
-        private ICollection<T> _collection;
-
         // Summary:
         //     Initializes a new instance of the System.Linq.EnumerableQuery<T> class and
         //     associates the instance with an expression tree.
@@ -25,25 +24,21 @@ namespace SharpArch.Domain.PersistenceSupport
         // Parameters:
         //   enumerable:
         //     A collection to associate with the new instance.
-        public BaseRepository(IEnumerable<T> enumerable)
-            : base(enumerable)
-        {
-            _collection = new List<T>(enumerable);
-        }
+        public BaseRepository(IEnumerable<T> enumerable) : base(enumerable) { }
 
         public virtual T this[object id]
         {
-            get { return (T)_collection.Cast<IEntityWithTypedId<object>>().Single(x => x.Id == id); }
+            get { return (T)this.Cast<IEntityWithTypedId<object>>().Single(x => x.Id == id); }
         }
 
         public virtual bool Contains(T item)
         {
-            return _collection.Contains(item);
+            return this.Contains<T>(item);
         }
 
         public virtual int Count
         {
-            get { return _collection.Count(); }
+            get { return this.Count<T>(); }
         }
 
         public virtual bool IsReadOnly
@@ -58,7 +53,8 @@ namespace SharpArch.Domain.PersistenceSupport
 
         public virtual void CopyTo(T[] array, int arrayIndex)
         {
-            _collection.CopyTo(array, arrayIndex);
+            var source = ((IEnumerable<T>)this).ToArray();
+            Array.Copy(source, 0, array, arrayIndex, source.Count());
         }
 
         public abstract void Add(T item);
--- Solutions/SharpArch.NHibernate/NHRepository.cs
+++ Solutions/SharpArch.NHibernate/NHRepository.cs
@@ -1,31 +1,54 @@
 namespace SharpArch.NHibernate
 {
 
+    using System;
+    using System.Linq;
     using global::NHibernate;
     using global::NHibernate.Linq;
-
     using SharpArch.Domain.PersistenceSupport;
 
     public class NHRepository<T> : BaseRepository<T>
     {
         public NHRepository() : base(NHibernateSession.CurrentFor<T>().Query<T>()) { }
 
-        protected virtual ISession Session {
+        protected virtual ISession Session
+        {
             get { return NHibernateSession.CurrentFor<T>(); }
         }
 
-        public override T this[object id] {
-            get {
+        public override T this[object id]
+        {
+            get
+            {
                 var entity = Session.Get<T>(id);
                 return entity;
             }
         }
 
+        public override int Count
+        {
+            get { return Session.Query<T>().Count(); }
+        }
+
+        public override bool Contains(T item)
+        {
+            return Session.Query<T>().Contains<T>(item);
+        }
+
+        public override void CopyTo(T[] array, int arrayIndex)
+        {
+            var source = Session.Query<T>().ToArray();
+            Array.Copy(source, 0, array, arrayIndex, source.Count());
+        }
+
         public override void Add(T item)
         {
-            try {
+            try
+            {
                 this.Session.Save(item);
-            } catch {
+            }
+            catch
+            {
                 if (this.Session.IsOpen)
                     this.Session.Close();
 
